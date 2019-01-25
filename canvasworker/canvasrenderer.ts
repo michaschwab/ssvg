16ae29faf188cd5c409e0a74d5f7b0faddcf37fa
michaschwab <me@michaschwab.de>
2019-01-25 14:33:34 -0500

    Go back to the basics on the canvas renderer, simplifying the code, and use the scale factor correctly.

--- canvasworker/canvasrenderer.ts
+++ canvasworker/canvasrenderer.ts
@@ -5,9 +5,6 @@ import SvgToCanvasWorker from "./canvasworker";
 export default class Canvasrenderer implements SvgToCanvasWorker {
     
     private ctx: CanvasRenderingContext2D;
-    private queues: { circles: any } = {
-        circles: {}
-    };
     
     constructor(private vdom: VDom, private canvas: HTMLCanvasElement) {
         const ctx = canvas.getContext('2d');
@@ -15,6 +12,8 @@ export default class Canvasrenderer implements SvgToCanvasWorker {
         
         this.ctx = ctx;
         this.ctx.scale(this.vdom.data.scale, this.vdom.data.scale);
+        console.log(this.vdom.data.scale);
+        this.ctx.save();
         
         this.draw();
         
@@ -27,37 +26,23 @@ export default class Canvasrenderer implements SvgToCanvasWorker {
     
     draw() {
         const ctx = this.ctx;
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-        let scale = this.vdom.data.scale;
+        
+        ctx.restore();
+        ctx.save();
         
         ctx.fillStyle = '#fff';
-        ctx.fillRect(0, 0, this.vdom.data.width * scale, this.vdom.data.height * scale);
+        ctx.fillRect(0, 0, this.vdom.data.width, this.vdom.data.height);
         
-        //this.executeSetAttributeQueue();
-        //ctx.save();
-        //console.log(this.visData);
         this.drawChildren(this.vdom.data);
-        //ctx.restore();
-        //ctx.drawImage(offscreenCanvas, 0, 0);
-        this.finishDrawingChildren();
         
         postMessage({msg: 'DRAWN'});
     }
     
-    private count = 0;
     private drawChildren(elData: any) {
         const ctx = this.ctx;
         
-        this.count++;
-        if(this.count < 4) {
-            //console.log(this.visData);
-        }
-        
-        //if(elData.type !== 'line')
-        {
-            ctx.save();
-            this.applyTransform(elData.transform);
-        }
+        ctx.save();
+        this.applyTransform(elData.transform);
         
         if(elData.type && elData.type !== 'g') {
             if(elData.type === 'title') {
@@ -67,28 +52,8 @@ export default class Canvasrenderer implements SvgToCanvasWorker {
             let stroke = elData.style.stroke ? elData.style.stroke : elData.stroke;
             let strokeWidth = elData.style['stroke-width'] ? elData.style['stroke-width'] : elData['stroke-width'];
             
-            if(this.lastDrawn && this.lastDrawn.type !== elData.type) {
-                if(this.lastDrawn.type === 'line') {
-                    //let path = new Path2D(currentD);
-                    ctx.closePath();
-                    ctx.stroke();
-                    ctx.restore(); //test
-                    ctx.restore();
-                } else if(this.lastDrawn.type === 'circle') {
-                    /*ctx.fill();
-                    ctx.stroke();
-                    console.log('circle end kind of?!');*/
-                }
-                ctx.closePath();
-            }
-            
             if(elData.type === 'circle') {
                 if(!fill) fill = '#000';
-                /*if(!this.queues.circles[fill]) {
-                    this.queues.circles[fill] = [];
-                }
-                this.queues.circles[fill].push(elData);*/
-                
                 ctx.beginPath();
                 ctx.fillStyle = DrawingUtils.colorToRgba(fill, elData.style['fill-opacity']);
                 ctx.strokeStyle = stroke;
@@ -98,26 +63,16 @@ export default class Canvasrenderer implements SvgToCanvasWorker {
                     ctx.stroke();
                 }
             } else if(elData.type === 'line') {
-                if(!this.lastDrawn || this.lastDrawn.type !== 'line') {
-                    ctx.save();
-                    this.applyTransform(elData.transform);
-                    
-                    ctx.beginPath();
-                    ctx.strokeStyle = stroke;
-                    //ctx.lineWidth = strokeWidth;
-                    //currentD = '';
-                }
-                
-                //ctx.beginPath();
+                ctx.beginPath();
+                ctx.strokeStyle = stroke;
                 ctx.moveTo(elData.x1, elData.y1);
                 ctx.lineTo(elData.x2, elData.y2);
-                //ctx.stroke();
+                ctx.stroke();
                 //currentD += 'M ' + elData.x1 +',' + elData.y1 + 'L ' + elData.x2 + ',' + elData.y2;
             } else if(elData.type === 'path') {
                 let p = new Path2D(elData.d);
                 //ctx.stroke(p);
                 ctx.fillStyle = fill;
-                //console.log(elData);
                 //ctx.fill(p);
                 if(stroke !== 'none') {
                     ctx.lineWidth = strokeWidth;
@@ -140,39 +95,8 @@ export default class Canvasrenderer implements SvgToCanvasWorker {
         }
         if(elData.type !== 'line') {
             //console.log(elData.type);
-            ctx.restore();
+            //ctx.restore();
         }
-    }
-    
-    private finishDrawingChildren() {
-        //console.log('finishing children');
-        //ctx.closePath();
-        //ctx.fill();
-        //ctx.stroke();
-        
-        for(let fill in this.queues.circles) {
-            if(this.queues.circles.hasOwnProperty(fill)) {
-                this.ctx.fillStyle = fill;
-                let sampleData = (this.queues.circles as any)[fill][0];
-                let stroke = sampleData.style.stroke ? sampleData.style.stroke : sampleData.stroke;
-                this.ctx.lineWidth = sampleData.strokeWidth;
-                this.ctx.strokeStyle = stroke;
-                //console.log(queues.circles[fill][0].stroke);
-                this.ctx.beginPath();
-                for(let elData of (this.queues.circles as any)[fill]) {
-                    this.ctx.moveTo(elData.cx + Math.round(elData.r), elData.cy);
-                    this.ctx.arc(elData.cx, elData.cy, elData.r, 0, 2 * Math.PI);
-                }
-                this.ctx.fill();
-                
-                if(stroke) {
-                    this.ctx.stroke();
-                }
-            }
-        }
-        
-        this.queues.circles = {};
-        this.lastDrawn = null;
     }
     
     private applyTransform(transformString: string) {
