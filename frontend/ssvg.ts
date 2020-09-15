d2d9f4fa31e2a3a371689edabf76652adacd7acb
michaschwab <me@michailschwab.com>
2020-09-15 18:06:07 -0400

    forgot to remove a console log, oups.

--- frontend/ssvg.ts
+++ frontend/ssvg.ts
@@ -418,8 +418,6 @@ export default class SSVG {
         };
     }
 
-    logged = 0;
-    
     private replaceD3Attr() {
 
         const me = this;
@@ -455,8 +453,6 @@ export default class SSVG {
                     // earlier versions, where selectAll() returned other values.
                     let elements = this._groups ? this._groups[0] : this[0];
 
-
-
                     if(typeof elements === 'object' && Object.keys(elements).length === 1 && elements.parentNode) {
                         const parentElement = elements.parentNode;
                         let parentNode: VdomNode;
@@ -475,10 +471,6 @@ export default class SSVG {
                         for(const child of parentNode.children) {
                             elements.push(me.domHandler.getElementFromNode(child));
                         }
-                    }
-                    if(name === 'opacity' && me.logged < 10) {
-                        me.logged++;
-                        console.log(elements, arguments, this, {...this._groups});
                     }
                     if(!elements) {
                         return originalFct.apply(this, arguments);
